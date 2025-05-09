1d51c80e2c886eeaaab2d07c9cd3fb36715ae2db
Gabriele <pepsy.gabry@gmail.com>
2025-05-09 08:51:55 +0200

    Update PMC.py

--- PMT/PMT.py
+++ PMT/PMT.py
@@ -10,24 +10,32 @@ data = read_agilent_binary(filename, include_time_vector=True)
 
 x = data['channel_2']['x_data']
 y = data['channel_2']['y_data']
+y_neg = np.where(y<0, y, 0)
+y_pos = np.where(y>0, y, 0)
 
 height_tresh = 0.007
 width_scale = 0.000000002
 
-peaks, _ = find_peaks(abs(y), height=height_tresh)
+peaks_neg, _ = find_peaks(abs(y_neg), height=height_tresh)
+peaks_pos, _ = find_peaks(abs(y_pos), height=height_tresh)
 
 # Numero di picchi
-print('Numero di picchi:', len(peaks))
-# DA AGGIUNGRE: numero di picchi NEGATIVI
+print('Numero di picchi negativi:', len(peaks_neg))
+print('Numero di picchi positivi:', len(peaks_pos))
 
 # Intensità
-intensity = np.array(y[peaks])
-avg_intensity = np.mean(abs(intensity)) # DA SISTEMARE, STO PRENDENDO SIA NEG CHE POS!
-print('Intensità media dei picchi:', avg_intensity)
+intensity_neg = np.array(y[peaks_neg])
+avg_intensity_neg = np.mean(abs(intensity_neg))
+intensity_pos = np.array(y[peaks_pos])
+avg_intensity_pos = np.mean(abs(intensity_pos))
+print('Intensità media dei picchi negativi:', avg_intensity_neg)
+print('Intensità media dei picchi positivi:', avg_intensity_pos)
 
 
-# fig, ax = plt.subplots()
-# ax.plot(x, y, zorder=0)
+fig, ax = plt.subplots()
+ax.plot(x, y, zorder=0)
+ax.scatter(x[peaks_neg], y[peaks_neg], color='yellow', zorder=1)
+ax.scatter(x[peaks_pos], y[peaks_pos], color='red', zorder=1)
 
-# plt.show()
+plt.show()
 
