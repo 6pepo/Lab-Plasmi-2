ed8eee66124724afdda06df6be617c8bfcb8591f
Alberto260699 <albysala99@gmail.com>
2025-05-22 22:00:17 +0200

    Add files via upload

--- Silicio/silicio.py
+++ Silicio/silicio.py
@@ -1,56 +1,179 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.optimize import curve_fit
-from scipy.signal import find_peaks
+import scipy.optimize as opt
+import scipy.signal as sig
 import os
+import fnmatch
 
-def gaussian(x,a,mean,sigma):
-    return a*np.exp(-((x-mean)**2/(sigma**2))/2)
+def gauss(x,A,mu,sigma):
+    exp=-(x-mu)**2/(2*sigma**2)
+    norm=A/(sigma*np.sqrt(2*np.pi))
+    return norm*np.exp(exp)
+def retta(x,m,q):
+    return x*m+q
 
-def line(x,m,q):
-    return m*x+q
+def option0(): # Calibrazione
+    var = str(input('Quale file vuoi caricare: '))
+    fl = var + '.mca'
+    cwd = os.getcwd()
+    
+    # Load data
+    os.chdir(cwd + '/Dati')
+    x=np.loadtxt(fl, usecols=(0), unpack=True, skiprows=12, max_rows=4096)
+    
+    # Find local max 
+    energy_peaks = np.array([6.9, 2.5], dtype=float)
+    x_peaks = np.array([42,8], dtype=float)
+    
+    # Plot data
+    fig=plt.figure()
+    ax1=fig.add_subplot(111)
+    ax1.scatter(x_peaks, energy_peaks, label='Data', color='blue')
+    popt, pcov = opt.curve_fit(retta, x_peaks, energy_peaks)
+    perr = np.sqrt(np.diag(pcov))
+    ax1.plot(x_peaks, retta(x_peaks, *popt), label='y={:.3e}*x + {:.3e}'.format(popt[0],popt[0]), color='red')
 
-file = 'S4.mca'
+    plt.xlabel('Channel (Ch)')
+    plt.ylabel('Energy (KeV)')
+    plt.legend()
+    plt.grid(linestyle='--')
+    #plt.savefig(var+'.png')
+    plt.show()
 
-#load data
-data = np.genfromtxt(file, delimiter=None, skip_header=12, skip_footer=72, unpack=True)
+def option1(): # Plot dei dati + salvataggio FWHM, H
+    var = str(input('Quale file vuoi caricare: '))
+    fl = var + '.mca'
+    cwd = os.getcwd()
+    
+    # Load data
+    os.chdir(cwd + '/Dati')
+    x=np.loadtxt(fl, usecols=(0), unpack=True, skiprows=12, max_rows=4096)
+    
+    # Find local max 
+    mask = (x[1:-1] >= x[:-2]) & (x[1:-1] >= x[2:]) & (x[1:-1] > 2)
+    x_peaks = x[1:-1][mask] # Picchi
+    x_idx_peaks = [i for i, x_max in enumerate(x) if x_max in x_peaks] # Indici dei picchi
+    mask1 = x > 2
+    x_mask = x[mask1]
+    x_idx_mask = [i for i, x_max in enumerate(x) if x_max in x_mask] # Indici della mashera
+    idx_left = min(x_idx_mask)
+    
+    # Plot data
+    fig=plt.figure()
+    ax1=fig.add_subplot(111)
+    ax1.plot(x, label='Data')
+    for i in range(len(x_peaks)):
+        result = [x_idx_mask[i] for i in range(len(x_idx_mask) - 1) if (x_idx_mask[i+1] - x_idx_mask[i] > 1) and (x_idx_mask[i] > idx_left)]
+        result1 = [x_idx_mask[i+1] for i in range(len(x_idx_mask) - 1) if (x_idx_mask[i+1] - x_idx_mask[i] > 1) and (x_idx_mask[i] > idx_left)]
+        
+        # Set idx_right
+        if not result:
+            idx_right = max(x_idx_mask)
+        else:
+            idx_right = result[0]
+        
+        if idx_left >= max(x_idx_mask):
+            pass
+        else:
+            x_fit = x[idx_left:idx_right]
+            x_range = np.linspace(idx_left, idx_right, idx_right - idx_left)
 
-data_info = np.genfromtxt(file, delimiter=';', dtype=str, skip_header=4110, skip_footer=17, unpack=True)
-shaping_time = data_info[0,2]
-shaping_time = float(shaping_time.split('=')[1].strip())
-gain = data_info[0,4]
-gain = float(gain.split('=')[1].strip())
-print(f'Shaping time: {shaping_time} mus\tgain: {gain}\tConteggi: {np.sum(data)}')
+            popt, pcov = opt.curve_fit(gauss, x_range, x_fit)
+            perr = np.sqrt(np.diag(pcov))
 
-peaks,info = find_peaks(data, threshold=2, prominence=3, width=2, distance=None)
-# print(info)
+            ax1.plot(x_range, gauss(x_range, *popt), label='Fit')
+            
+            idx_FWHM_min = [idx for idx in x_idx_mask if idx < x_idx_peaks[i]]
+            idx_FWHM_max = [idx for idx in x_idx_mask if idx > x_idx_peaks[i]]
+            FWHM_min = min(idx_FWHM_min, key=lambda idx: abs(x[idx] - x_peaks[i]/2))
+            FWHM_max = min(idx_FWHM_max, key=lambda idx: abs(x[idx] - x_peaks[i]/2))
+            
+            # Save results
+            for file in os.listdir(cwd + '/Dati'):
+                if fnmatch.fnmatchcase(file, 'G*.mca'):
+                    fl1=open('Results_gain.txt', 'a')
+                    fl1.write(str(var)+', '+str(i)+', '+str(x_peaks[i])+', '+str(FWHM_max-FWHM_min)+'\n')
+                    fl1.close()
+                elif fnmatch.fnmatchcase(file, 'S*.mca'):
+                    fl1=open('Results_shaping.txt', 'a')
+                    fl1.write(str(var)+', '+str(i)+', '+str(x_peaks[i])+', '+str(FWHM_max-FWHM_min)+'\n')
+                    fl1.close
+    
+            # Update idx_left if possible
+            if result1:
+                idx_left = result1[0]
+            else:
+                break  # No other segment to analyze
+            
+    #plt.title('')
+    plt.xlabel('Channel (Ch)')
+    plt.ylabel('Counts')
+    plt.legend()
+    plt.grid(linestyle='--')
+    #plt.savefig(var+'.png')
+    plt.show()
+    
+def option2(): # Plot dei dati salvati 
+    var = str(input('Quale file vuoi caricare: '))
+    fl = var + '.txt'
+    cwd = os.getcwd()
+    
+    # Load dati
+    os.chdir(cwd + '/Dati')
+    file_name, num_peak, peak, FWHM, gain = np.loadtxt(fl, usecols=(0,1,2,3,4), unpack=True, skiprows=1)
+    
+    fig = plt.figure()
+    ax=fig.add_subplot(111)
+    
+    # Set collection
+    gain0 = [gain[i] for i in range(len(file_name)) if num_peak[i] == 0]
+    gain1 = [gain[i] for i in range(len(file_name)) if num_peak[i] == 1]
+    peak0 = [peak[i] for i in range(len(file_name)) if num_peak[i] == 0]
+    peak1 = [peak[i] for i in range(len(file_name)) if num_peak[i] == 1]
+    FWHM0 = [FWHM[i] for i in range(len(file_name)) if num_peak[i] == 0]
+    FWHM1 = [FWHM[i] for i in range(len(file_name)) if num_peak[i] == 1]
+    
+    for i in range(len(file_name)):
+        if num_peak[i] == 0:
+            aux0_res.append(peak[i])
+        elif num_peak[i] == 1:
+            aux1_res.append(peak[i])
+            
+    # Plot data
+    ax.errorbar(gain, FWHM/peak, label='Data')
+    ax.errorbar(gain0, FWHM0/peak0 , label='First Peak')
+    ax.errorbar(gain1, FWHM1/peak1 , label='Second Peak')
+    if var == 'Results_gain':
+        plt.xlabel('Gain')
+    else:
+        plt.xlabel('Shaping')
+    plt.ylabel('Resolution')
+    plt.legend()
+    plt.grid(linestyle='--')
+    #plt.savefig(var+'.png')
+    plt.show()
+    
+#program
+OPTIONS = {
+    0: option0,
+    1: option1,
+    2: option2,
+}
 
-alpha_peak = 5.9 #KeV
-beta_peak = 6.5 #KeV
-energy_peaks = [alpha_peak, beta_peak]
+print("0 - calibrazione")
+print("1 - plot grafico + scrive su file")
+print("2 - plot grafici guadagno/shaping time + salvataggio immagine")
+print("\n")
 
-fig, ax = plt.subplots()
-ax.plot(data, color='blue', label='Conteggi')
-ax.plot(peaks, data[peaks], 'rx', label='peaks')
-ax.set_xlim(0,1000)
-ax.legend()
-
-fig1, ax1 = plt.subplots()
-popt,pcov = curve_fit(line, peaks, energy_peaks) #, maxfev=10000)
-perr = np.sqrt(np.diag(pcov))
-
-m = popt[0]
-err_m = perr[0]
-q = popt[1]
-err_q = perr[1]
-
-ax1.plot(peaks, energy_peaks, 'rx', linestyle='dashed', label=f'm:{m:.4f}, q: {q:.4f}')
-ax1.set_xlabel('Conteggi')
-ax1.set_ylabel('Energia (KeV)')
-ax1.legend()
-print(f'm:{m:.4f}, q: {q:.4f}')
-
-max_channel = 4096
-print(f'E_max: {m*max_channel+q:4f} KeV')
-
-plt.show()
+while True:
+    try:
+        option=int(input("Quale opzione vuoi eseguire:"))
+        if option in OPTIONS:
+            OPTIONS[option]()
+            break
+        else:
+            print("Errore: inserire un'opzione valida.")
+    except ValueError:
+        pass
+    finally:
+        True
