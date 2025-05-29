a976abdcebbc97cd12da9d727f1236f3b5ca908d
giovi <giova.l9708@gmail.com>
2025-05-29 15:39:40 +0200

    Unknown substance

--- Bromuro/bromuro.py
+++ Bromuro/bromuro.py
@@ -10,13 +10,37 @@ def gaussian(x,a,mean,sigma):
 def line(x,m,q):
     return m*x+q
 
-file = f'Dati Bromuro/fondo1_1000V/RAW/CH7@DT5730SB_2289_EspectrumR_fondo1_1000V_20250528_094644.txt3'
+def find_FWHM(a):
+    x_idx = [i for i, x in enumerate(a) if x in a] # Indicizza il vettore
+    peak = max(a) # Trova il valore del picco
+    idx_peak = [i for i, x in enumerate(a) if x == max(a)] 
+    idx_FWHM_min = [idx for idx in x_idx if idx < idx_peak[0]]
+    idx_FWHM_max = [idx for idx in x_idx if idx > idx_peak[0]]
+    FWHM_min = min(idx_FWHM_min, key=lambda idx: abs(a[idx] - peak/2))
+    FWHM_max = min(idx_FWHM_max, key=lambda idx: abs(a[idx] - peak/2))
+    return FWHM_max-FWHM_min
 
-#load data
-data = np.genfromtxt(file, delimiter=' ', skip_header=2, usecols=(0,1), unpack=True)
-channels = data[0]
-counts = data[1]
+file_fondo = r'Dati Bromuro/fondo1_1000V/RAW/CH7@DT5730SB_2289_EspectrumR_fondo1_1000V_20250528_094644.txt3'
+file_cesio = r'Dati Bromuro/cesio1_1000V/RAW/CH7@DT5730SB_2289_EspectrumR_cesio1_1000V_20250528_102235.txt3'
+file_sodio = r'Dati Bromuro/sodio22_1000V/RAW/CH7@DT5730SB_2289_EspectrumR_sodio22_1000V_20250528_104413.txt3'
+file_ignoto = r'Dati Bromuro/ignota_1000V/RAW/CH7@DT5730SB_2289_EspectrumR_ignota_1000V_20250528_110606.txt3'
 
+#bromuro di lantanio
+peak0_la = 789 #kev   #beta
+peak1_la = 1440 #kev   #cattura elettronica 
+peak2_la = 1472 #kev   #cattura elettronica
+
+#cesio
+peak_ce = 662 #kev
+
+#sodio
+peak_na = 511 #kev
+
+energy_peaks = [peak0_la, peak1_la, peak2_la, peak_ce, peak_na]
+channels_peaks = []
+
+#file fondo (Bromuro di Lantanio)
+channels, counts = np.genfromtxt(file_fondo, delimiter=' ', skip_header=2, usecols=(0,1), unpack=True)
 
 fig, ax = plt.subplots()
 
@@ -24,6 +48,7 @@ mask_ec = (channels >= 1080) & (channels <= 1180) #cattura elettronica
 ax.plot(channels,counts, label='Data')
 ax.set_xlabel('Channels')
 ax.set_ylabel('Counts')
+ax.set_title('$LaBr_{3}$ Spectrum')
 
 mask1 = (channels >= 1080) & (channels <= 1120)
 mask2 = (channels > 1120) & (channels <= 1180)
@@ -35,12 +60,11 @@ par2 = [2100., 1133., 5.]
 popt2, pcov2 = curve_fit(gaussian, channels[mask2], counts[mask2], p0=par2)
 
 x = np.linspace(np.min(channels[mask_ec]), np.max(channels[mask_ec]), 100)
-ax.plot(x, gaussian(x,*popt1), 'r--', label=f'Peak-1440: A = {popt1[0]:.2f}, $\mu$ = {popt1[1]:.2f}, $\sigma$ = {popt1[2]:.2f}')
-ax.plot(x, gaussian(x,*popt2), 'g--', label=f'Peak-1472: A = {popt2[0]:.2f}, $\mu$ = {popt2[1]:.2f}, $\sigma$ = {popt2[2]:.2f}')
+ax.plot(x, gaussian(x,*popt1), 'r--', label=f'EC Peak-1440: A = {popt1[0]:.2f}, $\mu$ = {popt1[1]:.2f}, $\sigma$ = {popt1[2]:.2f}')
+ax.plot(x, gaussian(x,*popt2), 'g--', label=f'EC Peak-1472: A = {popt2[0]:.2f}, $\mu$ = {popt2[1]:.2f}, $\sigma$ = {popt2[2]:.2f}')
 
 mask_beta = (channels >= 600) & (channels <= 850)
 filt_counts = savgol_filter(counts[mask_beta],20,1)
-ax.plot(channels[mask_beta], filt_counts, 'y--', label='Sagov')
 
 d2 = np.gradient(np.gradient(filt_counts))
 i = 0
@@ -53,37 +77,97 @@ while True:
         i = i+1
 
 index_flex = 600 + index_flex
-ax.plot(channels[mask_beta], d2, label='Derivata')
+ax.vlines(index_flex, 0, counts[index_flex-1]+10, colors='m', label=f'Beta Peak: {index_flex}')
 
 ax.grid(True)
 ax.legend()
 
-peaks0 = 789 #kev
-peak1 = 1440 #kev
-peak2 = 1472 #kev
+channels_peaks.extend([index_flex,popt1[1], popt2[1]])
 
-energy_peaks = [peaks0,peak1,peak2]
-channels_peaks = [index_flex,popt1[1], popt2[1]]
+
+#file cesio
+channels, counts = np.genfromtxt(file_cesio, delimiter=' ', skip_header=2, usecols=(0,1), unpack=True)
 
 fig1, ax1 = plt.subplots()
-popt_energy, pcov_energy = curve_fit(line, channels_peaks, energy_peaks)
 
-ax1.plot(channels_peaks, energy_peaks, 'rx', label='Peaks')
-x = np.linspace(np.min(channels_peaks), np.max(channels_peaks), 100)
-ax1.plot(x, line(x,*popt_energy), 'b--', label=f'm={popt_energy[0]:.2f}, q={popt_energy[1]:.2f}')
+ax1.plot(channels,counts, label='Data')
 ax1.set_xlabel('Channels')
-ax1.set_ylabel('Energy [keV]')
+ax1.set_ylabel('Counts')
+ax1.set_title('Ce-133 Spectrum')
+
+mask = (channels >= 480) & (channels <= 540)
+
+par = [50000., 510., 10]
+popt, pcov = curve_fit(gaussian, channels[mask], counts[mask], p0=par)
+
+x = np.linspace(np.min(channels[mask]), np.max(channels[mask]), 100)
+ax1.plot(x, gaussian(x,*popt), 'r--', label=f'Peak: A = {popt[0]:.2f}, $\mu$ = {popt[1]:.2f}, $\sigma$ = {popt[2]:.2f}')
+
 ax1.grid(True)
 ax1.legend()
 
-# Conversione post calibrazione --> Alberto
-energy = channels * popt_energy[0] + popt_energy[1]
+channels_peaks.extend([popt[1]])
+
+
+#file sodio
+channels, counts = np.genfromtxt(file_sodio, delimiter=' ', skip_header=2, usecols=(0,1), unpack=True)
+
 fig2, ax2 = plt.subplots()
-ax2.plot(energy, counts, 'rx', label='Data')
-plt.title('Conversione in energia))
-ax2.set_xlabel('Energy [keV]')
+
+ax2.plot(channels,counts, label='Data')
+ax2.set_xlabel('Channels')
 ax2.set_ylabel('Counts')
-ax2.grid('--')
+ax2.set_title('Na-22 Spectrum')
+
+mask = (channels >= 380) & (channels <= 410)
+
+par = [5000., 395., 10]
+popt, pcov = curve_fit(gaussian, channels[mask], counts[mask], p0=par)
+
+x = np.linspace(np.min(channels[mask]), np.max(channels[mask]), 100)
+ax2.plot(x, gaussian(x,*popt), 'r--', label=f'Peak: A = {popt[0]:.2f}, $\mu$ = {popt[1]:.2f}, $\sigma$ = {popt[2]:.2f}')
+
+ax2.grid(True)
 ax2.legend()
+
+channels_peaks.extend([popt[1]])
+
+
+fig_ene, ax_ene = plt.subplots()
+popt_energy, pcov_energy = curve_fit(line, channels_peaks, energy_peaks)
+
+ax_ene.plot(channels_peaks, energy_peaks, 'rx', label='Peaks')
+x = np.linspace(np.min(channels_peaks), np.max(channels_peaks), 100)
+ax_ene.plot(x, line(x,*popt_energy), 'b--', label=f'm={popt_energy[0]:.2f}, q={popt_energy[1]:.2f}')
+ax_ene.set_xlabel('Channels')
+ax_ene.set_ylabel('Energy [keV]')
+ax_ene.grid(True)
+ax_ene.legend()
+
+m = popt_energy[0]
+q = popt_energy[1]
+
+#sostanza ignota
+channels, counts = np.genfromtxt(file_ignoto, delimiter=' ', skip_header=2, usecols=(0,1), unpack=True)
+energy = channels*m+q
+
+fig3, ax3 = plt.subplots()
+
+ax3.plot(energy,counts, label='Data')
+ax3.set_xlabel('Energy [keV]')
+ax3.set_ylabel('Counts')
+ax3.set_title('The Substance Spectrum')
+ax3.grid(True)
+ax3.legend()
+
+peaks, info = find_peaks(counts, height=2e3, prominence=2e3)
+peaks = np.delete(peaks, [0,-1])
+peaks_markers = ['r*', 'g^']
+
+for i,p in enumerate(peaks):
+    ax3.plot(energy[p], counts[p],peaks_markers[i], label=f'Peak Energy: {energy[p]:.2f} keV')
+
+ax3.grid(True)
+ax3.legend()
 
 plt.show()
